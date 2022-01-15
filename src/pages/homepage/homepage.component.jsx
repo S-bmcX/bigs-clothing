6d25ff2534db847bb10ff359c344bc462bdf423a
bford <NehdenDev@Nehdens-Air.lan>
2022-01-14 21:32:59 -0600

    completed section 73

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./cstmHomepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+import '../../App.css';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default Homepage;
