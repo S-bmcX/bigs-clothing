7bf963efc1455213b8463c8b23fbffd540d82c06
bford <NehdenDev@Nehdens-Air.lan>
2022-01-18 19:40:22 -0600

    added cart icon and cart dropDown components to project, with all functionality and reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 // TO UNDERSTAND WHY THE ABOVE WAS DONE, READ HERE -> https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files
 // short version: imports file destination as a react component as opposed to its source content. This is unique for SVG w/ React
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to="/" className='logo-container'>
             <Logo className='logo' />
@@ -24,12 +26,18 @@ const Header = ({ currentUser }) => (
                 :
                 (<Link className='option' to='/signin'>Sign In</Link>)
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+            // this allows cart window visible/hidden function
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import userReducer from "./user/user-reducer";
+import cartReducer from './cart/cart-reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
 
 // this combines a pre-built redux function with the userReducer I built to create 1 object that has all the redux functionality intended for use
