c38c990b3eff13ac23716fcaa813d35417034219
bford <NehdenDev@Nehdens-Air.lan>
2022-02-01 20:37:37 -0600

    completed portions pertaining to routing to different areas of app in regards to specific shop items as well as normalizing data in regards to making shopdata an object globally but then converting that object into an array for the initial preview of shop items

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import {createSelector} from 'reselect';
+// import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
@@ -6,3 +7,15 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+    // this is intended to pull shop.data object -> convert to an array then push for the /shop/ preview page within collections-overview.
+);
+
+export const selectCollection = collectionUrlParam => 
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    );
